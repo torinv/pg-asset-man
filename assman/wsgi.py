cf5511574c1a62dc73613747fbd42506f4247f8a
Torin <tvermettewps@gmail.com>
2020-01-29 17:48:45 -0700

    updated and settings for whitenoise 5.1

--- assman/wsgi.py
+++ assman/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assman.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
