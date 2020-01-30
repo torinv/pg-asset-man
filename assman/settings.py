eaf59a49cbbeb7a5be065a5ba7ac72c2efdd34cf
Torin <tvermettewps@gmail.com>
2020-01-29 17:44:02 -0700

    updated staticfiles_storage

--- assman/settings.py
+++ assman/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Password validation
