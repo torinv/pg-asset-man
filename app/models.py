919e1ba2a5fd7a57d6f1c486516c156fdbd35ba8
Torin <tvermettewps@gmail.com>
2020-02-07 14:08:54 -0700

    working implementation with items only

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from crum import get_current_user
 
 class Item(models.Model):
 	item_name = models.CharField(max_length=50)
@@ -7,13 +8,28 @@ class Item(models.Model):
 	item_qty = models.IntegerField(default=1)
 	item_longdescription = models.CharField(max_length=300)
 	item_id = models.AutoField(primary_key=True)
+	item_created_by = models.ForeignKey('auth.User', null=True, blank=True, on_delete=models.SET_NULL, related_name='creator')
+	item_created_time = models.DateTimeField(auto_now_add=True)
+	item_last_edited_by = models.ForeignKey('auth.User', null=True, blank=True, on_delete=models.SET_NULL, related_name='last_editor')
+	item_last_edit_time = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.item_name
 
 	def get_absolute_url(self):
 		return reverse("item_edit", kwargs={"pk": self.pk})
-	
+
+	def save(self, *args, **kwargs):
+		user = get_current_user()
+		if user and not user.pk:
+			user = None
+		if not self.pk:
+			self.item_created_by = user
+		else:
+			self.item_last_edited_by = user
+		super(Item, self).save(*args, **kwargs)
+
+
 
 class Key(models.Model):
 	OWNERS = (
